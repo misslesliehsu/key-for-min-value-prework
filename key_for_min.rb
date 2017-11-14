8ab736352ad1592d50e3d162fe082c343513ae7e
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-14 15:46:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,8 @@
 def key_for_min_value(name_hash)
   min_key = nil
-  min_value = nil
+  min_value = Float::INFINITY
   name_hash.each do |key, value|
-    if value <= nil
-      min_value = value
-    elsif value < min_value
+    if value < min_value
       min_value = value
       min_key = key
     else
