2e6d6b8bf59fe6810cdd972980732b1c0c59af31
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-14 15:52:20 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,11 @@
 def key_for_min_value(name_hash)
   min_key = nil
-  min_value = Float::INFINITY
+  min_value = nil
   name_hash.each do |key, value|
-    if value < min_value
+    if min_value == nil
+      min_value = value
+      min_key = key
+    elsif value < min_value
       min_value = value
       min_key = key
     else
