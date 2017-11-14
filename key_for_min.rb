00e5b3395c88f135490a0d2314586c84bb46e7c4
Leslie Hsu <miss.leslie.hsu@gmail.com>
2017-11-14 15:48:22 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.each do |key, value|
-    if min_value = 0
+    if min_value == nil
       min_value = value
     elsif value < min_value
       min_value = value
